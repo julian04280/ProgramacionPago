a53ae13cc585c22f3a02f681a95dee6a2c2f9c50
julian04280 <julian04280@gmail.com>
2020-07-21 21:53:12 -0500

    Actualizar Proyecto Funcionando

---
+++ src/main/java/co/com/cliente/ProgramacionPago/julian/diaz/ProgramacionPagoApplication.java
@@ -0,0 +1,13 @@
+package co.com.cliente.ProgramacionPago.julian.diaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramacionPagoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramacionPagoApplication.class, args);
+	}
+
+}
